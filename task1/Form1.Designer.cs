e0d9eb3eccb60b44eaa451c269b650a19ce0370a
aktivgo <aktivgo01062001@gmail.com>
2022-02-27 10:58:29 +0300

    Add form

--- task1/Form1.Designer.cs
+++ task1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace task1
+﻿namespace Task1
 {
     partial class Form1
     {
@@ -29,11 +29,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.paintButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // paintButton
+            // 
+            this.paintButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.paintButton.Location = new System.Drawing.Point(12, 65);
+            this.paintButton.Name = "paintButton";
+            this.paintButton.Size = new System.Drawing.Size(220, 50);
+            this.paintButton.TabIndex = 0;
+            this.paintButton.Text = "Построить";
+            this.paintButton.UseVisualStyleBackColor = true;
+            this.paintButton.Click += new System.EventHandler(this.paintButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 50);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Задание 1";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.paintButton);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Button paintButton;
 
         #endregion
     }
--- task1/Form1.cs
+++ task1/Form1.cs
@@ -1,20 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace task1
+namespace Task1
 {
     public partial class Form1 : Form
     {
+        private Graphics Graph;
+        private Pen MyPen;
+
         public Form1()
         {
             InitializeComponent();
+            Graph = CreateGraphics();
+            MyPen = new Pen(Color.Black);
+        }
+
+        private void paintButton_Click(object sender, EventArgs e)
+        {
+            const double xMin = -5;
+            const double xMax = 5;
+            const double step = 0.01;
+            
+            Graph.Clear(Color.White);
+            
+            int x0 = ClientSize.Width / 2;
+            int y0 = ClientSize.Height / 2;
+            double t = xMin;
+            double x = (t - Math.Sin(t));
+            double y = (1 - Math.Cos(t));
+            int x1 = (int)(x0 + x);
+            int y1 = (int)(y0 - y);
+            while (t < xMax)
+            {
+                t += step;
+                x = (t - Math.Sin(t));
+                y =(1 - Math.Cos(t));
+                int x2 = (int)(x0 + x);
+                int y2 = (int)(y0 - y);
+                Graph.DrawLine(MyPen, x1, y1, x2, y2);
+                x1 = x2;
+                y1 = y2;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MyPen.Dispose();
+            Graph.Dispose();
         }
     }
 }
